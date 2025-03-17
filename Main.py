d1662f61fb615078fce33845b7ceb25da0e8483b
Usuario-Rodolfo <srrodolfo00!gmail.com>
2025-03-17 14:22:03 -0300

    Atualização da classe Main

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
+class Main:
+    pass
+
 print("Testando o projeto")
+
+from Cliente import Cliente
+from Conta import Conta
+
+# Criando um cliente
+c1 = Cliente("João", "114444-2222")
+
+# Criando uma conta associada ao cliente
+conta = Conta(c1.nome, 6565, 0)
+
+# Exibindo os detalhes da conta
+print(f"{conta.titular} - Número: {conta.numero} - Seu Saldo: {conta.saldo}")
