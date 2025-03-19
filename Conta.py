cec76a95600b4329e14a64ee1c5a58fa78991c3d
Usuario-Rodolfo <srrodolfo00!gmail.com>
2025-03-19 19:40:26 -0300

    Adicionando Main.py

--- Conta.py
+++ Conta.py
@@ -1,16 +1,29 @@
 class Conta:
-    def __init__(self, titular, numero, saldo):
-        self.saldo=0
+    def __init__(self, titular, numero, saldo=0):
+        self._saldo = saldo  # Inicializando corretamente o atributo privado
         self.numero = numero
         self.titular = titular
 
-        @property
-        def saldo(self):
-            return self._saldo
+    @property
+    def saldo(self):
+        return self._saldo
 
-        @saldo.setter
-        def saldo(self, saldo):
-            if saldo < 0:
-                print("O saldo não pode ser negativo")
-            else:
-                self._saldo = saldo
+    @saldo.setter
+    def saldo(self, saldo):
+        if saldo < 0:
+            print("O saldo não pode ser negativo")
+        else:
+            self._saldo = saldo
+
+    def saque(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            print("Saque realizado com sucesso")
+        else:
+            print("Saldo insuficiente")
+
+    def deposito(self, valor):
+        self.saldo += valor
+
+    def extrato(self):
+        print(f"Cliente: {self.titular} | Saldo Atual: {self.saldo}")
--- Main.py
+++ Main.py
@@ -1,13 +1,16 @@
+class Main:
+    pass
+
 from Cliente import Cliente
 from Conta import Conta
-
-print("Testando o projeto")
 
 # Criando um cliente
 c1 = Cliente("João", "114444-2222")
 
-# Criando uma conta associada ao cliente
-conta = Conta(c1.get_nome(), 6565, 0)
+# Criando uma conta para o cliente
+conta = Conta(c1.get_nome(), 1222, 0)
 
-# Exibindo os detalhes da conta
-print(f"{conta.titular} - Número: {conta.numero} - Seu Saldo: {conta.saldo}")
+conta.deposito(100)
+conta.saque(50)
+conta.extrato()
+
