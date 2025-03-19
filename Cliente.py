42c461fff1eb48404c80f8fe024bcb1085c377d7
Usuario-Rodolfo <srrodolfo00!gmail.com>
2025-03-19 18:26:51 -0300

    Adicionando Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,5 +1,12 @@
 class Cliente:
     def __init__(self, n, fone):
+        self._nome = n
+        self._telefone = fone
 
-        self.nome = n
-        self.telefone = fone
+    # método get
+    def get_nome(self):
+        return self._nome
+
+    # método set
+    def set_nome(self, nome):
+        self._nome = nome
--- Main.py
+++ Main.py
@@ -1,16 +1,13 @@
-class Main:
-    pass
-
-print("Testando o projeto")
-
 from Cliente import Cliente
 from Conta import Conta
+
+print("Testando o projeto")
 
 # Criando um cliente
 c1 = Cliente("João", "114444-2222")
 
 # Criando uma conta associada ao cliente
-conta = Conta(c1.nome, 6565, 0)
+conta = Conta(c1.get_nome(), 6565, 0)
 
 # Exibindo os detalhes da conta
 print(f"{conta.titular} - Número: {conta.numero} - Seu Saldo: {conta.saldo}")
